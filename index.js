0a691780e587ea76fc9eb4af0510a76ba4156ac1
natoine <antoineseilles@gmail.com>
2019-01-20 18:50:30 +0100

    now connect to local db and insert 10 patates when starting node

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var express = require('express')
 var app = express()
 
+const MongoClient = require('mongodb').MongoClient
+// Connection URL
+const url = 'mongodb://localhost:27017'
+// Database Name
+const dbName = 'shoppinglist'
+
+
+MongoClient.connect(url, function(err, client) {
+    if (err) throw err
+    console.log("Connected successfully to server")
+   
+    const db = client.db(dbName)
+    db.collection('items').insertOne({"label":"patate", "quantity":"10"}, function(err, res) {
+        if (err) throw err
+        console.log("1 item inserted")
+        client.close()
+      })
+    
+    //client.close()
+  })
+
+
 app.get('/', 
     function (request, response) 
     {
